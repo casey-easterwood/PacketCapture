eec9a529e16f72694d374e39d4cc7cdf8ccc4d6c
casey easterwood <casey.easterwood@gmail.com>
2022-04-06 19:28:40 -0700

    fix parser

--- Packets/IPv4.cs
+++ Packets/IPv4.cs
@@ -21,16 +21,17 @@ namespace PacketCapture.Packets
         {
             var version             = (data[0] & 0xF0) >> 4; //Left 
             var IHL                 = (data[0] & 0x0F);      //Right
-            var DSCP                = (data[1] & 0xFC) >> 4; 
+            var DSCP                = (data[1] & 0xFC) >> 2; 
             var ECN                 = (data[1] & 0x03);      
             var TotalLength         = BitConverter.ToUInt16(data.Skip(2).Take(2).Reverse().ToArray());
             var Identification      = (BitConverter.ToUInt16(data.Skip(4).Take(2).Reverse().ToArray())) & 0x1FFF;
-            var Flags               = (data[6] & 0xC0) >> 6;
+            var Flags               = (data[6] & 0xE0) >> 5;
             var FragmentionOffset   = BitConverter.ToUInt16(data.Skip(6).Take(2).ToArray());
             var TimeToLive          = (data[9] & 0xFF00) >> 8;
             IPV4Protocol protocol   = (IPV4Protocol) (data[9] & 0x00FF);
             IPAddress Desination    = new(data.Skip(12).Take(4).ToArray());
             IPAddress Source        = new(data.Skip(16).Take(4).ToArray());
+            
 
             Fields["Version"]           = Convert.ToString(version, 2).PadLeft(4, '0');
             Fields["IHL"]               = IHL.ToString(); // Convert.ToString(IHL, 2).PadLeft(4, '0');
