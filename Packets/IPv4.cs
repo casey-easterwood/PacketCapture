a19dd9fcf46c0e8ac6e9d0bb300e5abf2a846095
casey easterwood <casey.easterwood@gmail.com>
2022-04-06 18:29:59 -0700

    updated parse function for IPv4

--- Packets/IPv4.cs
+++ Packets/IPv4.cs
@@ -19,31 +19,28 @@ namespace PacketCapture.Packets
 
         public void parse(byte[] data)
         {
-            byte[] VersionIHLData           = data.Take(1).ToArray();
-            byte[] DSCPECNData              = data.Skip(1).Take(1).ToArray();
-            byte[] TotalLengthData          = data.Skip(2).Take(2).Reverse().ToArray();
-            byte[] IdentificationData       = data.Skip(4).Take(2).Reverse().ToArray();
-            byte[] ProtocolData             = data.Skip(9).Take(1).ToArray();
-            byte[] DestinationAddressData   = data.Skip(12).Take(4).ToArray();
-            byte[] SourceAddressData        = data.Skip(16).Take(4).ToArray();
-
-            IPAddress Desination = new(DestinationAddressData);
-            IPAddress Source = new(SourceAddressData);
-
-            var version             = (VersionIHLData[0] & 0xF0) >> 4; //Left 
-            var IHL                 = (VersionIHLData[0] & 0x0F);      //Right
-            var DSCP                = (DSCPECNData[0] & 0xFC) >> 4; 
-            var ECN                 = (DSCPECNData[0] & 0x03);      
-            var TotalLength         = BitConverter.ToUInt16(TotalLengthData);
-            var Identification      = BitConverter.ToUInt16(IdentificationData);
-            IPV4Protocol protocol   = (IPV4Protocol) (ProtocolData[0] & 0x00FF);
+            var version             = (data[0] & 0xF0) >> 4; //Left 
+            var IHL                 = (data[0] & 0x0F);      //Right
+            var DSCP                = (data[1] & 0xFC) >> 4; 
+            var ECN                 = (data[1] & 0x03);      
+            var TotalLength         = BitConverter.ToUInt16(data.Skip(2).Take(2).Reverse().ToArray());
+            var Identification      = (BitConverter.ToUInt16(data.Skip(4).Take(2).Reverse().ToArray())) & 0x1FFF;
+            var Flags               = (data[6] & 0xC0) >> 6;
+            var FragmentionOffset   = BitConverter.ToUInt16(data.Skip(6).Take(2).ToArray());
+            var TimeToLive          = (data[9] & 0xFF00) >> 8;
+            IPV4Protocol protocol   = (IPV4Protocol) (data[9] & 0x00FF);
+            IPAddress Desination    = new(data.Skip(12).Take(4).ToArray());
+            IPAddress Source        = new(data.Skip(16).Take(4).ToArray());
 
             Fields["Version"]           = Convert.ToString(version, 2).PadLeft(4, '0');
-            Fields["IHL"]               = Convert.ToString(IHL, 2).PadLeft(4, '0');
+            Fields["IHL"]               = IHL.ToString(); // Convert.ToString(IHL, 2).PadLeft(4, '0');
             Fields["DSCP"]              = Convert.ToString(DSCP, 2).PadLeft(6, '0');
             Fields["ECN"]               = Convert.ToString(ECN, 2).PadLeft(2, '0');
             Fields["TotalLength"]       = TotalLength.ToString();
             Fields["Identification"]    = Identification.ToString();
+            Fields["Flags"]             = Flags.ToString().PadLeft(2, '0');
+            Fields["FragmentionOffset"] = FragmentionOffset.ToString();
+            Fields["TimeToLive"]        = TimeToLive.ToString();
             Fields["Destination IP"]    = Desination.ToString();
             Fields["Source IP"]         = Source.ToString();
             Fields["Protocol"]          = protocol.ToString();
